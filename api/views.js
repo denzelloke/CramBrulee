b5d52eef0b864160e152100b3f6f5d7f55e126c0
timothy <timothyloh0523@ymail.com>
2024-06-19 00:48:20 +0800

    replaced res.render

--- api/views.js
+++ api/views.js
@@ -3,11 +3,27 @@ const hbs = require('hbs');
 
 const templatePath = path.join(__dirname, '../public/templates');
 
-export default (req, res) => {
-  if (req.method === 'GET') {
+// Configure hbs
+hbs.registerPartials(templatePath);
+
+const renderTemplate = (templateName, data) => {
+  return new Promise((resolve, reject) => {
+    hbs.renderFile(path.join(templatePath, `${templateName}.hbs`), data, (err, html) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(html);
+      }
+    });
+  });
+};
+
+export default async (req, res) => {
+  try {
     const viewName = req.url.slice(1) || 'login';
-    res.render(path.join(templatePath, viewName));
-  } else {
-    res.status(405).send('Method Not Allowed');
+    const html = await renderTemplate(viewName, {});
+    res.status(200).send(html);
+  } catch (error) {
+    res.status(500).send('Error rendering template');
   }
 };
